710ccf12807b5d3cd96c2d2e1f784cb2760695fb
Emil Oja <emil.oja@gmail.com>
2020-10-23 08:26:51 +0200

    chore: added deleteActor()

--- src/main/java/actorlibrary/Controllers/ActorController.java
+++ src/main/java/actorlibrary/Controllers/ActorController.java
@@ -2,7 +2,9 @@ package actorlibrary.Controllers;
 
 import actorlibrary.Models.Actor;
 import actorlibrary.Models.CommonResponse;
+import actorlibrary.Models.Movie;
 import actorlibrary.Repositories.ActorRepository;
+import actorlibrary.Repositories.MovieRepository;
 import actorlibrary.Utils.Command;
 import actorlibrary.Utils.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -11,15 +13,34 @@ import org.springframework.web.bind.annotation.*;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.net.URL;
+import java.util.ArrayList;
 import java.util.Optional;
+
+import com.mashape.unirest.http.HttpResponse;
+import com.mashape.unirest.http.Unirest;
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonParser;
 
 
 @RestController
 public class ActorController {
 
+    public static void main(String[]args) throws Exception{
+
+        getActorsMovies("https://www.imdb.com/name/nm0000216/");
+
+
+    }
 
     @Autowired
     private ActorRepository repository;
+
+
+
+
     @GetMapping("/")
     String hello(){
         return "Hello Hello";
@@ -77,8 +98,8 @@ public class ActorController {
         cr.message = "New actor with id: " + actor.id;
 
         HttpStatus resp = HttpStatus.CREATED;
-
         response.addHeader("Location", "/actor/" + actor.id);
+
 
         cmd.setResult(resp);
         Logger.getInstance().logCommand(cmd);
@@ -123,4 +144,68 @@ public class ActorController {
         Logger.getInstance().logCommand(cmd);
         return new ResponseEntity<>(cr, resp);
     }
+    @GetMapping
+    public ResponseEntity<CommonResponse> getMovieById(HttpServletRequest request, @PathVariable("id") Integer id) {
+        Command cmd = new Command(request);
+
+
+
+        //process
+        CommonResponse cr = new CommonResponse();
+        HttpStatus resp;
+
+        if (repository.existsById(id)) {
+            cr.data = repository.findById(id);
+            cr.message = "Author with id: " + id;
+            resp = HttpStatus.OK;
+        } else {
+            cr.data = null;
+            cr.message = "Author not found";
+            resp = HttpStatus.NOT_FOUND;
+        }
+
+        cmd.setResult(resp);
+        Logger.getInstance().logCommand(cmd);
+        return new ResponseEntity<>(cr, resp);
+    }
+
+    @DeleteMapping("api/v1/actor/{id}")
+    public ResponseEntity<CommonResponse> deleteActor(HttpServletRequest request, @PathVariable Integer id) {
+        Command cmd = new Command(request);
+
+        CommonResponse cr = new CommonResponse();
+        HttpStatus resp;
+
+        if(repository.existsById(id)) {
+            repository.deleteById(id);
+            cr.message = "Deleted actor with id: " + id;
+            resp = HttpStatus.OK;
+        } else {
+            cr.message = "Actor not found with id: " + id;
+            resp = HttpStatus.NOT_FOUND;
+        }
+
+        cmd.setResult(resp);
+        Logger.getInstance().logCommand(cmd);
+        return new ResponseEntity<>(cr, resp);
+    }
+
+    public static void getActorsMovies(String imdbUrl) throws Exception{
+        ArrayList<Movie> allMovies = new ArrayList<>();
+        String querry = "https://rapidapi.p.rapidapi.com/actors/get-all-filmography?nconst=" +imdbUrl.substring(26,35);
+
+
+        HttpResponse<String> response = Unirest.get(querry)
+                .header("x-rapidapi-host", "imdb8.p.rapidapi.com")
+                .header("x-rapidapi-key", "3ff559fc9dmsh90e7d601c0dcaa5p15bf2djsnd7c89f0e1067")
+                .asString();
+        //Prettifying
+        Gson gson = new GsonBuilder().setPrettyPrinting().create();
+        JsonParser jp = new JsonParser();
+        JsonElement je = jp.parse(response.getBody().toString());
+        String prettyJsonString = gson.toJson(je);
+        System.out.println(prettyJsonString);
+
+    }
+
 }
--- src/main/java/actorlibrary/Models/Actor.java
+++ src/main/java/actorlibrary/Models/Actor.java
@@ -1,13 +1,17 @@
 package actorlibrary.Models;
 
 
+import com.fasterxml.jackson.annotation.JsonGetter;
 import com.fasterxml.jackson.annotation.JsonIdentityInfo;
 import com.fasterxml.jackson.annotation.ObjectIdGenerators;
 
 import javax.persistence.*;
 import java.net.URL;
 import java.time.LocalDate;
-import java.util.Date;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
 
 @Entity
 @JsonIdentityInfo(
@@ -30,4 +34,13 @@ public class Actor {
 
     @Column
     public URL imdbURL;
+
+//    @JsonGetter("books")
+//    public List<String> movies() {
+//        return books.stream()
+//                .map(movie -> {
+//                    return "/book/" + book.id;
+//                }).collect(Collectors.toList());
+//    }
+
 }
